259bba74f28e40186b01fc087a708bdd2401e27d
Ivan Riera Sanchez <riera-sanchez@punkt.de>
2015-03-03 14:14:18 +0100

    [TASK] Namespace removed from MigrationsController.php

--- Classes/Controller/MigrationsController.php
+++ Classes/Controller/MigrationsController.php
@@ -24,7 +24,6 @@
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
-namespace PunktDe\PtMigrations\MigrationsController;
 
 use PunktDe\PtMigrations\Domain\Model\MigrationState;
 
